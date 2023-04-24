b967c50cc5233a6cdb70c9ec5500bb83d7f77f1f
Sampsonthecat <123033273+Sampsonthecat@users.noreply.github.com>
2023-04-23 23:17:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,12 @@ function shrinkHeader() {
    const header = document.querySelector('.animated-header');
 
   header.style.padding = '10px';
-  header.style.fontSize = '120px';
+  header.style.fontSize = '90px';
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeInText = document.getElementById("fade-in-text");  
+  setTimeout(function () {
+    fadeInText.style.opacity = "1";
+  }, 1100);
+});
